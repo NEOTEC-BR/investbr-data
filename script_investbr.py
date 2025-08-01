1d59fc71a928d11df784cef15219184529921b48
Carlos Henrique <ch-023@hotmail.com>
2025-08-01 19:40:18 -0300

    Atualização script

--- script_investbr.py
+++ script_investbr.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pytz
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -217,39 +218,39 @@ def buscar_dados_acao_fundamentus(ticker):
 # Lista de ações para consulta
 acoes = ["AALR3", "ABCB4", "ABEV3", "AERI3", "AFLT3", "AGRO3", "AGXY3", 
          "ALLD3", "ALOS3", "ALPA3", "ALPA4", "ALPK3", "ALUP11", "AMAR3", 
-         "AMBP3", "AMER3", "AMOB3", "ANIM3", "ARML3", "ASAI3", "ATMP3", "AURE3", 
-         "AVLL3", "AZUL4", "AZZA3", "B3SA3", "BALM4", "BAZA3", "BBAS3", "BBDC3", 
-         "BBDC4", "BBSE3", "BEEF3", "BEES3", "BEES4", "BGIP3", "BGIP4", "BHIA3", 
-         "BIED3", "BLAU3", "BMEB3", "BMEB4", "BMGB4", "BMKS3", "BMOB3", "BOBR4", 
-         "BPAC11", "BPAC3", "BPAC5", "BPAN4", "BRAP3", "BRAP4", "BRAV3", "BRFS3", 
-         "BRKM3", "BRKM5", "BRSR3", "BRSR6", "BRST3", "CAMB3", "CAML3", "CASH3", 
-         "CBAV3", "CCTY3", "CEAB3", "CEBR3", "CEBR6", "CEDO4", "CGAS5", "CGRA3", 
-         "CGRA4", "CLSC3", "CLSC4", "CMIG3", "CMIG4", "CMIN3", "COCE3", "COCE5", 
-         "COGN3", "CPFE3", "CPLE3", "CPLE6", "CRFB3", "CRPG5", "CSAN3", "CSED3", 
-         "CSMG3", "CSNA3", "CSUD3", "CURY3", "CVCB3", "CXSE3", "CYRE3", "DASA3", 
-         "DESK3", "DEXP3", "DEXP4", "DIRR3", "DMVF3", "DOTZ3", "DXCO3", "EALT3", 
-         "EALT4", "ECOR3", "EGIE3", "ELET3", "ELET5", "ELET6", "EMAE4", "EMBR3", 
-         "ENEV3", "ENGI11", "ENGI3", "ENGI4", "ENJU3", "EQPA3", "EQTL3", "ESPA3", 
-         "ETER3", "EUCA3", "EUCA4", "EVEN3", "EZTC3", "FESA3", "FESA4", "FHER3", 
-         "FIQE3", "FLRY3", "FRAS3", "GFSA3", "GGBR3", "GGBR4", "GGPS3", "GMAT3", 
-         "GOAU3", "GOAU4", "GRND3", "GUAR3", "HAPV3", "HBOR3", "HBSA3", "HYPE3", 
-         "IFCM3", "IGTI11", "INTB3", "IRBR3", "ISAE3", "ISAE4", "ITSA3", "ITSA4", 
-         "ITUB3", "ITUB4", "JALL3", "JHSF3", "JSLG3", "KEPL3", "KLBN11", "KLBN3", 
-         "KLBN4", "LAND3", "LAVV3", "LEVE3", "LIGT3", "LJQQ3", "LOGG3", "LOGN3", 
-         "LPSB3", "LREN3", "LVTC3", "LWSA3", "MATD3", "MDIA3", "MDNE3", "MEAL3", 
-         "MELK3", "MGLU3", "MILS3", "MLAS3", "MNDL3", "MOAR3", "MOTV3", "MOVI3", 
-         "MRVE3", "MTRE3", "MTSA4", "MULT3", "MYPK3", "NEOE3", "NEXP3", "NGRD3", 
-         "NTCO3", "ODPV3", "OFSA3", "OIBR3", "OIBR4", "ONCO3", "OPCT3", "ORVR3", 
-         "PCAR3", "PDTC3", "PETR3", "PETR4", "PETZ3", "PFRM3", "PGMN3", "PLAS3", 
-         "PLPL3", "PMAM3", "PNVL3", "POMO3", "POMO4", "PORT3", "POSI3", "PRIO3", 
-         "PRNR3", "PSSA3", "PTBL3", "PTNT3", "PTNT4", "QUAL3", "RADL3", "RAIL3", 
-         "RAIZ4", "RANI3", "RAPT3", "RAPT4", "RDNI3", "RDOR3", "REAG3", "RECV3", 
-         "RENT3", "ROMI3", "SANB11", "SAPR11", "SAPR3", "SAPR4", "SBFG3", "SBSP3", 
-         "SCAR3", "SEER3", "SEQL3", "SGPS3", "SHUL4", "SIMH3", "SLCE3", "SMFT3", 
-         "SMTO3", "SOJA3", "SRNA3", "STBP3", "SUZB3", "SYNE3", "TAEE11", "TAEE3", 
-         "TAEE4", "TASA3", "TASA4", "TCSA3", "TECN3", "TEND3", "TFCO4", "TGMA3", 
-         "TIMS3", "TOKY3", "TOTS3", "TRAD3", "TRIS3", "TTEN3", "TUPY3", "TXRX3", 
-         "UCAS3", "UGPA3", "UNIP3", "UNIP6", "USIM3", "USIM5", "USIM6", "VALE3", 
+        #  "AMBP3", "AMER3", "AMOB3", "ANIM3", "ARML3", "ASAI3", "ATMP3", "AURE3", 
+        #  "AVLL3", "AZUL4", "AZZA3", "B3SA3", "BALM4", "BAZA3", "BBAS3", "BBDC3", 
+        #  "BBDC4", "BBSE3", "BEEF3", "BEES3", "BEES4", "BGIP3", "BGIP4", "BHIA3", 
+        #  "BIED3", "BLAU3", "BMEB3", "BMEB4", "BMGB4", "BMKS3", "BMOB3", "BOBR4", 
+        #  "BPAC11", "BPAC3", "BPAC5", "BPAN4", "BRAP3", "BRAP4", "BRAV3", "BRFS3", 
+        #  "BRKM3", "BRKM5", "BRSR3", "BRSR6", "BRST3", "CAMB3", "CAML3", "CASH3", 
+        #  "CBAV3", "CCTY3", "CEAB3", "CEBR3", "CEBR6", "CEDO4", "CGAS5", "CGRA3", 
+        #  "CGRA4", "CLSC3", "CLSC4", "CMIG3", "CMIG4", "CMIN3", "COCE3", "COCE5", 
+        #  "COGN3", "CPFE3", "CPLE3", "CPLE6", "CRFB3", "CRPG5", "CSAN3", "CSED3", 
+        #  "CSMG3", "CSNA3", "CSUD3", "CURY3", "CVCB3", "CXSE3", "CYRE3", "DASA3", 
+        #  "DESK3", "DEXP3", "DEXP4", "DIRR3", "DMVF3", "DOTZ3", "DXCO3", "EALT3", 
+        #  "EALT4", "ECOR3", "EGIE3", "ELET3", "ELET5", "ELET6", "EMAE4", "EMBR3", 
+        #  "ENEV3", "ENGI11", "ENGI3", "ENGI4", "ENJU3", "EQPA3", "EQTL3", "ESPA3", 
+        #  "ETER3", "EUCA3", "EUCA4", "EVEN3", "EZTC3", "FESA3", "FESA4", "FHER3", 
+        #  "FIQE3", "FLRY3", "FRAS3", "GFSA3", "GGBR3", "GGBR4", "GGPS3", "GMAT3", 
+        #  "GOAU3", "GOAU4", "GRND3", "GUAR3", "HAPV3", "HBOR3", "HBSA3", "HYPE3", 
+        #  "IFCM3", "IGTI11", "INTB3", "IRBR3", "ISAE3", "ISAE4", "ITSA3", "ITSA4", 
+        #  "ITUB3", "ITUB4", "JALL3", "JHSF3", "JSLG3", "KEPL3", "KLBN11", "KLBN3", 
+        #  "KLBN4", "LAND3", "LAVV3", "LEVE3", "LIGT3", "LJQQ3", "LOGG3", "LOGN3", 
+        #  "LPSB3", "LREN3", "LVTC3", "LWSA3", "MATD3", "MDIA3", "MDNE3", "MEAL3", 
+        #  "MELK3", "MGLU3", "MILS3", "MLAS3", "MNDL3", "MOAR3", "MOTV3", "MOVI3", 
+        #  "MRVE3", "MTRE3", "MTSA4", "MULT3", "MYPK3", "NEOE3", "NEXP3", "NGRD3", 
+        #  "NTCO3", "ODPV3", "OFSA3", "OIBR3", "OIBR4", "ONCO3", "OPCT3", "ORVR3", 
+        #  "PCAR3", "PDTC3", "PETR3", "PETR4", "PETZ3", "PFRM3", "PGMN3", "PLAS3", 
+        #  "PLPL3", "PMAM3", "PNVL3", "POMO3", "POMO4", "PORT3", "POSI3", "PRIO3", 
+        #  "PRNR3", "PSSA3", "PTBL3", "PTNT3", "PTNT4", "QUAL3", "RADL3", "RAIL3", 
+        #  "RAIZ4", "RANI3", "RAPT3", "RAPT4", "RDNI3", "RDOR3", "REAG3", "RECV3", 
+        #  "RENT3", "ROMI3", "SANB11", "SAPR11", "SAPR3", "SAPR4", "SBFG3", "SBSP3", 
+        #  "SCAR3", "SEER3", "SEQL3", "SGPS3", "SHUL4", "SIMH3", "SLCE3", "SMFT3", 
+        #  "SMTO3", "SOJA3", "SRNA3", "STBP3", "SUZB3", "SYNE3", "TAEE11", "TAEE3", 
+        #  "TAEE4", "TASA3", "TASA4", "TCSA3", "TECN3", "TEND3", "TFCO4", "TGMA3", 
+        #  "TIMS3", "TOKY3", "TOTS3", "TRAD3", "TRIS3", "TTEN3", "TUPY3", "TXRX3", 
+        #  "UCAS3", "UGPA3", "UNIP3", "UNIP6", "USIM3", "USIM5", "USIM6", "VALE3", 
          "VAMO3", "VBBR3", "VITT3", "VIVA3", "VIVT3", "VLID3", "VSTE3", "VTRU3", 
          "VULC3", "VVEO3", "WEGE3", "WEST3", "WHRL3", "WHRL4", "WIZC3", "WLMM4", "YDUQ3"]
 
@@ -272,8 +273,9 @@ for acao in acoes:
     if "ticker" in dados_fund and "ticker" in dados_inv10:
         dados_combinados["ticker"] = dados_inv10["ticker"]
     
-    # Adiciona atualizado_em como último campo Usa UTC-3 (horário de Brasília) em vez de UTC
-    dados_combinados["atualizado_em"] = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+    # Força o uso da timezone de São Paulo (que representa o horário de Brasília)
+    brasilia_tz = pytz.timezone('America/Sao_Paulo')
+    dados_combinados["atualizado_em"] = datetime.now(brasilia_tz).strftime("%Y-%m-%d %H:%M:%S")
     
     dados_acoes.append(dados_combinados)
 
